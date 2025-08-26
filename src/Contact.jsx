cb7fe9162a4b1f4e72a38487a44f424675142c03
Sally M <smei201900@gmail.com>
2025-08-25 21:41:33 -0400

    added space

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,9 +3,9 @@ export default function ContactSection() {
     <div>
         <section id="contacts">
             <h1>Contact</h1>
-            <a href="https://github.com/SMei201900" id="profile-link" target="_blank">Github</a>
-            | 
-            <a href="https://www.linkedin.com/in/sally-mei-bird01" target="_blank">LinkedIn</a>
+              See more at <a href="https://github.com/SMei201900" id="profile-link" target="_blank">Github</a>  
+              &nbsp;|&nbsp;   
+              <a href="https://www.linkedin.com/in/sally-mei-bird01" target="_blank">LinkedIn</a>
         </section>
     </div>
   );
