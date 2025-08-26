ccd55d6988c51f3540dc89c9d793f9e8d8eb21f6
Sally M <smei201900@gmail.com>
2025-08-25 21:13:39 -0400

    created template to convert html-css-js-only into a React version

---
+++ src/Contact.jsx
@@ -0,0 +1,1 @@
+export const string = "Contact Section" 
---
+++ src/Projects.jsx
@@ -0,0 +1,8 @@
+const projects = 2; 
+
+export default function projectsAlso() {
+    return (
+        console.log(`${projects} , here are my projects`)
+    )
+}
+
