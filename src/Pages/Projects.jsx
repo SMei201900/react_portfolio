c949b294d53905f5693fe205199fea16fa8af400
Sally M <smei201900@gmail.com>
2025-09-01 18:39:55 -0400

    troubleshooting styling for mobile devices

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -40,7 +40,7 @@ export default function Projects() {
 
         <div className='carousel'>
             <button className='carouselBtn' onClick={prev}> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-left" viewBox="0 0 16 16">
                     <path d="M10 12.796V3.204L4.519 8zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753"/>
                 </svg>    
             </button>
@@ -60,7 +60,7 @@ export default function Projects() {
             </div>
 
             <button className='carouselBtn' onClick={next}> 
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right" viewBox="0 0 16 16">
                     <path d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
                 </svg> 
             </button>
