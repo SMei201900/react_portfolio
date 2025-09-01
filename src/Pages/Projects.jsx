7e88296e919d04eb06e1c9b981a1e31053634002
Sally M <smei201900@gmail.com>
2025-09-01 17:59:36 -0400

    okay figured out carousel but the styling is off

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,50 +1,62 @@
 import '../styling/Projects.css'
+import { useState } from 'react';
 
-function Projects() {
+const projectData = [ 
+{ 
+    title: "Survey Form",
+    link: "https://github.com/SMei201900/survey-form", 
+    img: "/assets/survey_icon_by_flaticon.png", 
+    alt: "Survey Icon",
+}, { 
+    title: "Book Store",
+    link: "https://codesandbox.io/p/sandbox/heuristic-cdn-997vmr", 
+    img: "/assets/bookstore.png", 
+    alt: "Bookstore Icon",
+}, { 
+    title: "Weather App",
+    link: "https://github.com/SMei201900/react-weather-project", 
+    img: "/assets/rosalind-franklin-time-magazine-cover.jpg", 
+    alt: "Cloud with the Sun peeking out behind it to represent the weather",
+}]
+
+export default function Projects() {
+    const [currentIndex, setIndex] = useState(0); 
+
+    const next = () => {
+        setIndex((prevIndex) => (prevIndex + 1) % projectData.length); 
+    }; 
+
+    const prev = () => {
+        setIndex( (prevIndex) => 
+        prevIndex === 0 ? projectData.length - 1 : prevIndex - 1 );  
+    }; 
+
+    const currentProject = projectData[currentIndex]; 
+
   return <div>
     <section id="projects">
         <h1>Projects</h1>
-        <div className="project-grid">
-            <div className="project-tile">
-                <div className="project-title">
+        <div className='carousel'>
+            <button className='carouselBtn' onClick={prev}> dot </button>
+            
+            <div className='project-tile carouselTile'>
+                <div className='project-title'>
                     <h3>
-                        <a href="https://codesandbox.io/p/sandbox/heuristic-cdn-997vmr"
-                        target="_blank"
-                        >Tribute Page</a>
+                        <a href={currentProject.link} target="_blank" rel="noreferrer">
+                            {currentProject.title}
+                        </a>
                     </h3>
                 </div>
-                <div className="project-image">
-                    <img 
-                    className="tribute-page-img" 
-                    src="/assets/rosalind-franklin-time-magazine-cover.jpg" 
-                    alt="Rosalin Franklin as the Cover of Time Magazine" /> 
+
+                <div className='project-image'>
+                    <img src={currentProject.img} alt={currentProject.alt} />
                 </div>
             </div>
 
-            <div className="project-tile">
-                <div className="project-title">
-                    <h3>
-                        <a href="https://codesandbox.io/p/sandbox/lingering-wave-k554lr?file=%2Findex.html" target="_blank">Survey</a>
-                    </h3>
-                </div>
-                <div className="project-image">
-                    <img src="/assets/survey_icon_by_flaticon.png" alt="Survey Icon" /> 
-                </div>
-            </div>
+            <button className='carouselBtn' onClick={next}> triangle </button>
 
-            <div className="project-tile">
-                <div className="project-title">
-                    <h3>
-                        <a href="https://github.com/SMei201900/bookstore" target="_blank">Bookstore</a>
-                    </h3>
-                </div>
-                <div className="project-image">
-                    <img src="/assets/bookstore.png" alt="Bookstore Icon" /> 
-                </div>
-            </div>
         </div>
     </section>
 </div>
 };
 
-export default Projects;
